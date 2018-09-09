2d45247078f78216ed54c12ea9b19afd8f5224bc
kudrykun <kudrykun@gmail.com>
2018-09-09 21:28:15 +0300

    Игрок, стрельба, начало работы над волнами врагов

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,5 +5,36 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour {
 
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
 
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
