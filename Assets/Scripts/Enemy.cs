e8130f23323b0597f31d63947692ef14e974c79f
kudrykun <kudrykun@gmail.com>
2018-09-10 22:42:37 +0300

    Начало работы с DamageDealer

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float health = 100;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        health -= damageDealer.GetDamage();
+    }
+}
